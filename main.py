b3f2491fad4b42ad25eee35178e8b3b0a1edd33b
Kee Archambault <kee.archambault@gmail.com>
2019-06-12 09:45:52 -0500

    pasted in html and added inputs

--- main.py
+++ main.py
@@ -3,10 +3,44 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = 
+form = '''
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+
+        <form method='post'>
+
+            <label for='rot_input'> Rotate by:</label>
+            <input id='rot_input' name='rot' type='text' value=0></input>
+            <input type='submit'></input>
+
+            <textarea name='text'></textarea>
+      
+      </form>
+    </body>
+</html>
+'''
 
 @app.route("/")
 def index(): 
-    return "Hello World"
+    return form
 
 app.run()    
