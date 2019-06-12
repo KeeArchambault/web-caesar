18bea5d209db67753da69c7edae7d06b456c8cc8
Kee Archambault <kee.archambault@gmail.com>
2019-06-12 10:18:25 -0500

    added encryption capability

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -42,5 +44,12 @@ form = '''
 @app.route("/")
 def index(): 
     return form
+
+@app.route('/', methods = ['post'])    
+def encrypt():
+    rot =  int(request.form['rot'])
+    text = request.form['text']
+    encrypt = rotate_string(text, rot)
+    return '<h1>' + encrypt + '</h1>'
 
 app.run()    
